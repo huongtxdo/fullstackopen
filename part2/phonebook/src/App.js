ff70f74714e5e264b8aa83b8c6e8fdf287ac4a02
Huong Laptop <huong.tx.do@gmail.com>
2022-09-19 12:11:04 +0300

    step6 of phonebook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,20 +1,22 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from 'axios'
 import { Filter } from "./components/Filter";
 import { PersonForm } from "./components/PersonForm";
 import { Persons } from "./components/Persons";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-    { name: "Huy", number: "046-123456" },
-    { name: "Huong", number: "046-987654" },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [searchname, setSearchName] = useState("");
+
+  useEffect(() => {
+    console.log('effect');
+    axios.get('http://localhost:3001/persons').then(response => {
+      console.log('promise fulfilled');
+      setPersons(response.data)
+    })
+  }, [])
 
   const addToPersons = (event) => {
     event.preventDefault();
@@ -25,8 +27,8 @@ const App = () => {
       setPersons(persons.concat(personObject));
       setNewName("");
       setNewNumber("");
-    }
-  };
+    } 
+  }; 
 
   const handleNewName = (event) => {
     setNewName(event.target.value);
