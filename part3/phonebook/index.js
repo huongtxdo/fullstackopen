bb9056a70f2e95764d651d43faa02634c65afe17
Huong pc <huong.tx.do@gmail.com>
2022-10-01 04:59:06 +0300

    add 3.7 and 3.8 to phonebook

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,93 +1,114 @@
-const express = require("express")
-const app = express()
-
-app.use(express.json())
+const { json } = require("express");
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
 
 let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
 
-app.get('/', (request, response) => {
-    response.send('<h1>Hello</h1>')
-})
+app.use(express.json());
 
-app.get('/info', (request, response) => {
-    const count = persons.length
-    const dateTime = new Date()
-    response.send(
-        `<p>Phonebook has info for ${count} people<p>
+morgan.token("body", function (req, res) {
+  return JSON.stringify(req.body);
+});
+
+app.use(
+  morgan(function (tokens, req, res) {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, "content-length"),
+      "-",
+      tokens["response-time"](req, res),
+      "ms",
+      tokens.body(req, res),
+    ].join(" ");
+  })
+);
+
+app.get("/", (request, response) => {
+  response.send("<h1>Hello</h1>");
+});
+
+app.get("/info", (request, response) => {
+  const count = persons.length;
+  const dateTime = new Date();
+  response.send(
+    `<p>Phonebook has info for ${count} people<p>
         <p>${dateTime}</p>`
-)})
+  );
+});
 
-app.get('/api/persons', (request, response) => {
-    response.json(persons)
-})
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-    if (person)
-        response.json(person)
-    else 
-        response.status(404).end()
-})
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((person) => person.id === id);
+  if (person) response.json(person);
+  else response.status(404).end();
+});
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-    
-    response.status(204).end()
-})
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((person) => person.id !== id);
+
+  response.status(204).end();
+});
 
 const generateId = () => {
-    return Math.floor(Math.random()*5000)
-}
+  return Math.floor(Math.random() * 5000);
+};
 
-app.post('/api/persons', (request, response) => {
-    const body = request.body
-    //missing name or number
-    if (!body.name || !body.number) {
-        return response.status(400).json({
-            error: 'name and number must not be empty'
-        })
-    }
-    //name already exists
-    const alreadyExist = persons.find(person => person.name === body.name)
-    if (alreadyExist) {
-        return response.status(400).json({
-            error: 'name must be unique'
-        })
-    }
-    //successful
-    const person = {
-        name: body.name,
-        number: body.number,
-        id: generateId()
-    }
-    persons = persons.concat(person)
-    response.json(person)
-})
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+  //missing name or number
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "name and number must not be empty",
+    });
+  }
+  //name already exists
+  const alreadyExist = persons.find((person) => person.name === body.name);
+  if (alreadyExist) {
+    return response.status(400).json({
+      error: "name must be unique",
+    });
+  }
+  //successful
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  };
+  persons = persons.concat(person);
+  response.json(person);
+});
 
-const PORT = 3001
+const PORT = 3001;
+
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
